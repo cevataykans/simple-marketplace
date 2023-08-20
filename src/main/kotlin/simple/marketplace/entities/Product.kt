dde54fdb8e175eaacfaebe63bf8eb741084423a9
cevataykans <aykansevinc66@gmail.com>
2023-08-20 20:27:35 +0200

    Build package by fixing compile time errors

--- src/main/kotlin/simple/marketplace/entities/Product.kt
+++ src/main/kotlin/simple/marketplace/entities/Product.kt
@@ -1,15 +1,17 @@
 package simple.marketplace.entities
 
 import io.micronaut.data.annotation.GeneratedValue
+import io.micronaut.data.annotation.Id
 import io.micronaut.data.annotation.MappedEntity
+import org.bson.types.ObjectId
 
+const val REPOSITORY = "products"
 
-@MappedEntity
+@MappedEntity(value = REPOSITORY)
 data class Product(
         @field:Id
         @GeneratedValue
-        val id: String,
+        val id: ObjectId,
         var name: String,
         var description: String,
-        var price: Double) {
-}
+        var price: Double)
--- src/main/kotlin/simple/marketplace/repositories/ProductRepository.kt
+++ src/main/kotlin/simple/marketplace/repositories/ProductRepository.kt
@@ -5,6 +5,8 @@ import io.micronaut.data.mongodb.annotation.MongoRepository
 import io.micronaut.data.repository.CrudRepository
 import org.bson.types.ObjectId
 
+import simple.marketplace.entities.Product
+
 @MongoRepository
 interface ProductRepository : CrudRepository<Product, ObjectId> {
 }
