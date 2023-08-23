9b13ba087a160ce9585a3bd55343b5f0854544e7
cevataykans <aykansevinc66@gmail.com>
2023-08-23 23:21:02 +0200

    Add schema for product

---
+++ src/main/resources/db/migration/V1__schema.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS product;
+
+CREATE TABLE product (
+    id          BIGSERIAL PRIMARY KEY,
+    name        VARCHAR(255) NOT NULL,
+    description TEXT NOT NULL,
+    price       DECIMAL NOT NULL
+);
