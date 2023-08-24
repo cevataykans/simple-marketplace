e7f830826e315ef76894911a9585870628682d81
cevataykans <aykansevinc66@gmail.com>
2023-08-24 21:04:01 +0200

    Add GET /{id} path

--- src/main/kotlin/simple/marketplace/controllers/ProductController.kt
+++ src/main/kotlin/simple/marketplace/controllers/ProductController.kt
@@ -1,11 +1,20 @@
 package simple.marketplace.controllers
 
+import io.micronaut.http.HttpResponse
 import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Get
 import io.micronaut.scheduling.TaskExecutors
 import io.micronaut.scheduling.annotation.ExecuteOn
+import simple.marketplace.entities.Product
 import simple.marketplace.services.ProductService
 
 @ExecuteOn(TaskExecutors.IO)
 @Controller("/products")
 open class ProductController(var service: ProductService) {
+
+    @Get("/{id}")
+    fun getById(id: Long): HttpResponse<Product> {
+        val found = service.getById(id)
+        return HttpResponse.ok(found)
+    }
 }
