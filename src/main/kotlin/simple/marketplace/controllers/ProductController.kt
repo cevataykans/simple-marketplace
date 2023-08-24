b7e0304df35fd1949c493389ab942a7a0ef9204d
cevataykans <aykansevinc66@gmail.com>
2023-08-24 21:13:22 +0200

    Add remaining PUT, DELETE, POST paths

--- src/main/kotlin/simple/marketplace/controllers/ProductController.kt
+++ src/main/kotlin/simple/marketplace/controllers/ProductController.kt
@@ -1,8 +1,8 @@
 package simple.marketplace.controllers
 
 import io.micronaut.http.HttpResponse
-import io.micronaut.http.annotation.Controller
-import io.micronaut.http.annotation.Get
+import io.micronaut.http.HttpStatus
+import io.micronaut.http.annotation.*
 import io.micronaut.scheduling.TaskExecutors
 import io.micronaut.scheduling.annotation.ExecuteOn
 import simple.marketplace.entities.Product
@@ -22,5 +22,35 @@ open class ProductController(var service: ProductService) {
     fun getAll(): HttpResponse<List<Product>> {
         val all = service.getAll()
         return HttpResponse.ok(all)
+    }
+
+    //TODO: make sure that product body is a valid Product, no field (name, id, description, price) must be deserialized!
+    //TODO: try making idempotent
+    @Post
+    fun create(@Body product: Product): HttpResponse<Product> {
+        if (product.id != null) {
+            return HttpResponse.badRequest()
+        }
+        val created = service.create(product)
+        return HttpResponse.ok(created)
+    }
+
+    //TODO: make sure that product body is a valid Product, no field (name, id, description, price) must be deserialized!
+    @Put("/{id}")
+    fun update(id: Long, @Body product: Product): HttpResponse<Product> {
+        if (product.id == null) {
+            return HttpResponse.badRequest()
+        }
+
+        if (id != product.id) {
+            return HttpResponse.badRequest()
+        }
+        return HttpResponse.ok(service.update(product))
+    }
+
+    @Delete("/{id}")
+    @Status(HttpStatus.NO_CONTENT)
+    fun delete(id: Long) {
+        service.delete(id)
     }
 }
