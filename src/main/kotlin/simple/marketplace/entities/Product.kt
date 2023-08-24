55a88d4d1853a2e05ce5db924765908c2e6f59eb
cevataykans <aykansevinc66@gmail.com>
2023-08-24 21:01:01 +0200

    Make product serializable

--- src/main/kotlin/simple/marketplace/entities/Product.kt
+++ src/main/kotlin/simple/marketplace/entities/Product.kt
@@ -3,9 +3,11 @@ package simple.marketplace.entities
 import io.micronaut.data.annotation.GeneratedValue
 import io.micronaut.data.annotation.Id
 import io.micronaut.data.annotation.MappedEntity
+import io.micronaut.serde.annotation.Serdeable
 
 const val REPOSITORY = "products"
 
+@Serdeable
 @MappedEntity(value = REPOSITORY)
 data class Product(
     @field:Id
@@ -15,11 +17,18 @@ data class Product(
     var description: String,
     var price: Float
 ) {
-
     override fun equals(other: Any?): Boolean {
         return (other is Product)
                 && this.name == other.name
                 && this.description == other.description
                 && this.price == other.price
+    }
+
+    override fun hashCode(): Int {
+        var result = id?.hashCode() ?: 0
+        result = 31 * result + name.hashCode()
+        result = 31 * result + description.hashCode()
+        result = 31 * result + price.hashCode()
+        return result
     }
 }
